aabc81d74a9f2fbfd893eea76d208cc34e458b64
SanitaryNest702 <gaeldzul240@gmail.com>
2026-06-27 16:52:42 -0600

    Create reportes.h

---
+++ reportes.h
@@ -0,0 +1,16 @@
+#ifndef REPORTES_H
+#define REPORTES_H
+
+extern int contadorReportes[6];
+
+void listarPeliculasMasRentadas();
+void listarClientesConMasRentas();
+void calcularPromedioDiasRenta();
+void calcularTotalMultas();
+void calcularIngresoTotalRentas();
+void listarRentasVencidas();
+void exportarReporte();
+void limpiarHistorial();
+void menuReportes();
+
+#endif
