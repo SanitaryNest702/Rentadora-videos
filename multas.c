4fe01fad7b5908101d69dbee5bc02d43e5dbd094
SanitaryNest702 <gaeldzul240@gmail.com>
2026-06-27 14:48:02 -0600

    Create multas.c

---
+++ multas.c
@@ -0,0 +1,196 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#define MAX_MULTAS 100
+struct Multa{
+int id;
+int idRenta;
+int idCliente;
+float monto;
+float montoPagado;
+int tipo; /*1=retraso 2=perdida*/
+int saldada;/*1=pagada 0=pendiente*/
+int activo;/*1=existe 0=eliminada*/
+};
+
+struct Multa multas[MAX_MULTAS];
+int totalMultas=0;
+
+void generarMultaRetraso(int idRenta, int idCliente, int diasRetraso){
+  if(totalMultas>=MAX_MULTAS){
+printf("No se pueden registrar mas multas\n");
+    return;
+  }
+if(diasRetraso<=0){
+  printf("Los dias de retraso deben ser mayores a 0\n"); 
+  return;
+}
+float tarifaDiaria=10.0;
+struct Multa nueva;
+nueva.id=totalMultas+1;
+nueva.idRenta=idRenta;
+nueva.idCliente=idCliente;
+nueva.monto=diasRetraso*tarifaDiaria;
+nueva.montoPagado=0;
+nueva.tipo=1;
+nueva.saldada=0;
+nueva.activo=1;
+multas[totalMultas]=nueva;
+totalMultas++;
+
+printf("Multa generada correctamene\n");
+printf("Dias de retraso: %d\n", diasRetraso);
+printf("Monto a pagar: %.2f\n", nueva.monto);
+}
+
+
+void generarMultaPerdida(int idRenta, int idCliente, float valorPelicula){
+  if(totalMultas>=MAX_MULTAS){
+    printf("No se pueden registrar más multas\n");
+    return;
+  }if(valorPelicula<=0){
+    printf("El valor de la pelicula tiene que ser mayor a 0\n");
+    return;
+  }
+  struct Multa nueva;
+  nueva.id=totalMultas+1;
+  nueva.idRenta=idRenta;
+  nueva.idCliente=idCliente;
+  nueva.monto=valorPelicula;
+  nueva.montoPagado=0;
+  nueva.tipo=2;/*2=Multa perdida*/
+  nueva.saldada=0;
+  nueva.activo=1;/*1=Existe*/
+
+  multas[totalMultas]=nueva;
+  totalMultas++;
+
+  printf("Multa por perdida generada correctamente.\n");
+  printf("Valor de la perdida: %.2f\n", nueva.monto); 
+  
+}
+
+void consultarMultaCliente(int idCliente){
+  int encontrado=0;
+  printf("===MULTAS PENDIENTES===\n");
+  for(int i=0;i<totalMultas;i++){
+    if(multas[i].activo==1 && multas[i].idCliente==idCliente && multas[i].saldada==0){
+      encontrado=1;
+      printf("\n ID Multa: %d\n", multas[i].id);
+      printf("\nID renta: %d\n", multas[i].idRenta);
+      printf("\nTipo: %s\n", multas[i].tipo==1? "Retraso": "Perdida de pelicula");
+      printf("Monto total: %.2f\n", multas[i].monto);
+      printf("Monto pagado: %.2f\n", multas[i].montoPagado);
+      printf("Saldo pendiente: %.2f\n", multas[i].monto-multas[i].montoPagado);/*Calcula el saldo pendiente en caso de pagos parciales*/
+      
+    }
+}
+if(encontrado==0){
+  printf("\nSin multas pendientes\n");
+  }
+}
+
+void registrarPagoMulta(){
+  int id;
+  printf("Ingrese el ID de la multa a pagar: ");
+  scanf("%d", &id);
+
+  int encontrado=0;
+  for(int i=0;i<totalMultas;i++){
+    if(multas[i].activo==1 && multas[i].id==id){
+      encontrado=1;
+
+      if(multas[i].saldada==1){
+        printf("Esta multa ya fue saldada\n");
+        return;
+      }
+      float saldoPendiente= multas[i].monto-multas[i].montoPagado;
+      printf("\nID multa: %d\n", multas[i].id);
+      printf("Monto Total: %.2f\n", multas[i].monto);
+      printf("Monto pagado: %.2f\n", multas[i].montoPagado);
+      printf("Saldo Pendiente: %.2f\n", multas[i].monto- multas[i].montoPagado);
+
+      float pago;
+      printf("Ingrese el monto a pagar: $");
+      scanf("%f", &pago);
+
+      if(pago<=0){
+        printf("EL monto debe ser mayor a 0\n"); 
+        return;
+      }if(pago>saldoPendiente){/*Evitar que pague mas de lo que debe*/
+        printf("El monto ingresado supera el saldo pendiente\n");
+        return;
+      }
+      multas[i].montoPagado+=pago; /*Acumula pagos parciales*/
+      if(multas[i].montoPagado>=multas[i].monto){/*Verifica si ya quedo saldada completamente*/
+        multas[i].saldada=1;
+        printf("Multa saldada correctamente\n");
+      }else{
+        printf("Pago parcial registrado\n");
+        printf("Saldo pendiente: %.2f\n", multas[i].monto-multas[i].montoPagado);
+      }
+      break;
+      
+    }
+  }
+  if(encontrado==0){
+    printf("Multa no encontrada\n");
+  }
+}
+
+
+void menuMultas(){
+  int opcion;
+do{
+  printf("\n===MODULO DE MULTAS===\n"); 
+  printf("1.Generar multa por entrega tardía\n"); 
+  printf("2.Generar multa por perdida de pelicula\n");
+  printf("3.Consultar multas pendiendes del cliente\n");
+  printf("4.Registrar pago de multa\n");
+  printf("0.Regresar al menu principal\n");
+  printf("Opcion: "); 
+  scanf("%d", &opcion);
+  switch(opcion){
+    case 1:{
+    int idRenta, idCliente, diasRetraso;
+    printf("ID del renta: ");
+    scanf("%d", &idRenta); 
+    printf("ID del cliente: ");
+    scanf("%d", &idCliente);
+    printf("Dias de retraso: ");
+    scanf("%d", &diasRetraso);
+    generarMultaRetraso(idRenta, idCliente, diasRetraso);
+    break;
+    }
+
+    case 2:{
+      int idRenta, idCliente;
+      float valorPelicula;
+      printf("ID de la renta: ");
+      scanf ("%d", &idRenta);
+      printf("ID del cliente: "); 
+      scanf("%d", &idCliente);
+      printf("Valor de reposicion de pelicula: ");
+      scanf("%f", &valorPelicula);
+      generarMultaPerdida(idRenta, idCliente, valorPelicula);
+    break;
+    }
+    case 3:{
+      int idCliente;
+      printf("ID del cliente: ");
+      scanf("%d", &idCliente);
+      consultarMultaCliente(idCliente);
+    break;
+    }
+    case 4:{
+      registrarPagoMulta();
+    break;
+    }
+    case 0:
+      printf("Regresando al menu principal...\n");
+    break;
+    default:
+    printf("Opcion invalida\n");
+  }
+}while(opcion!=0);
+}
