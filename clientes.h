e6cdf5d3c8c525d2d1645c46e2987bd684b99ff5
SanitaryNest702 <gaeldzul240@gmail.com>
2026-07-03 17:20:24 -0600

    Update clientes.h

--- clientes.h
+++ clientes.h
@@ -1,13 +1,15 @@
 #ifndef CLIENTES_H
 #define CLIENTES_H
 #define MAX_CLIENTES 100
+
 struct Cliente{
-int id;
-char nombre[50];
-char telefono[50];
-char direccion[100];
-int activo;
+	int id;
+	char nombre[50];
+	char telefono[50];
+	char direccion[100];
+	int activo;
 };
+
 extern struct Cliente clientes[MAX_CLIENTES];
 extern int totalClientes;
 void registrarClientes();
