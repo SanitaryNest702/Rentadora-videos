b236ca08b8f123bcb6519cbe6159d36357ba3c0c
SanitaryNest702 <gaeldzul240@gmail.com>
2026-07-03 17:17:33 -0600

    Update reportes.h

--- reportes.h
+++ reportes.h
@@ -1,8 +1,6 @@
 #ifndef REPORTES_H
 #define REPORTES_H
 
-extern int contadorReportes[6];
-
 void listarPeliculasMasRentadas();
 void listarClientesConMasRentas();
 void calcularPromedioDiasRenta();
